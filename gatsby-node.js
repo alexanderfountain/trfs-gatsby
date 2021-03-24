db18ba0bb13739584431a23adc62aaea497e8e43
Alexander Fountain <fountain@AlexandersMBP2.PK5001Z>
2021-03-24 11:06:03 -0500

    gatsby node revert

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
-// const path = require("path")
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   const pages = await graphql(`
-//     {
-//       page: allPrismicPage {
-//         nodes {
-//           uid
-//         }
-//       }
-//     }
-//   `)
-//   const pageTemplate = path.resolve("src/templates/page.js")
-//   pages.data.page.nodes.forEach(node => {
-//     if (node.uid == "home") {
-//       createPage({
-//         path: `/`,
-//         component: pageTemplate,
-//         context: {
-//           uid: node.uid,
-//         },
-//       })
-//     } else {
-//       createPage({
-//         path: `/${node.uid}`,
-//         component: pageTemplate,
-//         context: {
-//           uid: node.uid,
-//         },
-//       })
-//     }
-//   })
-// }
+const path = require("path")
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const pages = await graphql(`
+    {
+      page: allPrismicPage {
+        nodes {
+          uid
+        }
+      }
+    }
+  `)
+    const pageTemplate = path.resolve("src/templates/page.js")
+    pages.data.page.nodes.forEach(node => {
+        if (node.uid == "home") {
+            createPage({
+                path: `/`,
+                component: pageTemplate,
+                context: {
+                    uid: node.uid,
+                },
+            })
+        } else {
+            createPage({
+                path: `/${node.uid}`,
+                component: pageTemplate,
+                context: {
+                    uid: node.uid,
+                },
+            })
+        }
+    })
+}
 
 // const { createFilePath } = require(`gatsby-source-filesystem`)
 // exports.onCreateNode = ({ node, actions, getNode }) => {
